7e0c7d5a3d78ce6789f9a4a9773a092f245f21e1
vmkiss <veronika.kiss17@gmail.com>
2024-06-24 17:29:35 -0700

    write function to create square grid with dynamic number of rows and columns

--- sketch.js
+++ sketch.js
@@ -1,12 +1,29 @@
 // Get container holding etch-a-sketch grid
-container = document.querySelector(".container");
+let container = document.querySelector(".container");
 container.style.backgroundColor = "blue";
-// Create 16x16 grid and append each square to container
-for (let i = 0; i <= 15; i++) {
-    newDiv = document.createElement("div");
-    newDiv.style.backgroundColor = "red";
-    newDiv.style.width = "100px";
-    newDiv.style.height = "100px";
-    newDiv.style.border = "solid black";
-    container.appendChild(newDiv);
+
+//Create function to draw a square grid with given number of rows and columns
+function drawGrid(numRows) {
+    for (let i = 0; i < numRows ** 2; i++) {
+        let newDiv = document.createElement("div");
+        let divWidth = 800 / numRows - 2;
+        newDiv.style.width = divWidth + "px";
+        newDiv.style.height = divWidth + "px";
+        newDiv.style.border = "solid black 1px";
+        container.appendChild(newDiv);
+    }
 }
+
+drawGrid(100);
+
+// Create 16x16 grid and append each square to container
+/*for (let i = 0; i < 256; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.style.backgroundColor = "red";
+    let divHeight = 960/16 - 2;
+    let divWidth = 960/16 - 2;
+    newDiv.style.width = divWidth + "px";
+    newDiv.style.height = divHeight + "px";
+    newDiv.style.border = "solid black 1px";
+    container.appendChild(newDiv);
+} */
