a3eb5df5f2b689cd9aa060c27865488b8564e2fa
vmkiss <veronika.kiss17@gmail.com>
2024-06-24 18:07:24 -0700

    create clear all button

--- sketch.js
+++ sketch.js
@@ -1,20 +1,45 @@
 // Get container holding etch-a-sketch grid
 let container = document.querySelector(".container");
-container.style.backgroundColor = "blue";
 
-//Create function to draw a square grid with given number of rows and columns
+// Create function to draw a square grid with given number of rows and columns
 function drawGrid(numRows) {
     for (let i = 0; i < numRows ** 2; i++) {
         let newDiv = document.createElement("div");
-        let divWidth = 800 / numRows - 2;
+        let divWidth = 700 / numRows - 2;
+
+        // Style squares
         newDiv.style.width = divWidth + "px";
         newDiv.style.height = divWidth + "px";
         newDiv.style.border = "solid black 1px";
-        container.appendChild(newDiv);
+        
+         // Make squares change color when user hovers over them
+         newDiv.addEventListener("mouseover", () => {
+            newDiv.style.backgroundColor = "black";
+         })
+         
+         container.appendChild(newDiv);
     }
 }
 
-drawGrid(100);
+// Allow user to clear board and greate new grid by pressing clear all button
+clearBtn = document.querySelector(".clear-btn");
+clearBtn.addEventListener("click", () => {
+    userNum = prompt("Enter a number between 0 and 100")
+    if (userNum < 0 || userNum > 100) {
+        alert("Please enter a number between 0 and 100!")
+    } else {
+        container.remove();
+        drawGrid(userNum);
+    }
+
+})
+
+// Draw default 16x16 grid
+drawGrid(16);
+
+
+
+
 
 // Create 16x16 grid and append each square to container
 /*for (let i = 0; i < 256; i++) {
